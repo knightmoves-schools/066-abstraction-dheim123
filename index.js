023fad71334d9c89a1cfb70716928cda1c307774
dheim123 <dheim@midland.k12.ia.us>
2025-11-25 10:42:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-class TaxCalculator{
-    
-    constructor(){
-        this.rate = 15;
+class TaxCalculator {
+    #rate;
+
+    constructor() {
+        this.#rate = 15;
         this.state = 'TX';
         this.exempt = false;
     }
+
+    #calculateExempt(value) {
+        return value * 0.5;
+    }
+
+    #calculateNonExempt(value) {
+        return value * (1 + this.#rate / 100);
+    }
     
-    calculate(){
-        if(this.exempt){
-            return calculateExempt(1.37);
-        }else{
-            return calculateNonExempt(5.72);
+    calculate() {
+        if (this.exempt) {
+            return this.#calculateExempt(1.37);
+        } else {
+            return this.#calculateNonExempt(5.72);
         }
     }
 }
+
